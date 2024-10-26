5c16c4dcc19bc08198c23acd697c2ccead0c77e5
Haizlbliek <linus.denning@gmail.com>
2024-10-25 21:08:47 -0400

    Add Rails Project to Git

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  image_url   :string
+#  price       :decimal(8, 2)
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Product < ApplicationRecord
 	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
