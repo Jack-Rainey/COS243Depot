d759c72b2d1f49db96f087c7b896b3f47c95c12d
Haizlbliek <linus.denning@gmail.com>
2024-10-28 09:49:10 -0400

    Added lots of stuff

--- app/models/product.rb
+++ app/models/product.rb
@@ -18,4 +18,20 @@ class Product < ApplicationRecord
 		message: 'must be a URL for GIF, JPEG, or PNG image.'
 	}
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+	#...
+
+	private
+
+	# Ensure that there are no lines referencing this product
+	def ensure_not_referenced_by_any_line_item
+		unless line_items.empty?
+			errors.add(:base, 'Line Items present')
+			throw :abort
+		end
+	end
 end
