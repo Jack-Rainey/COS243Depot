6e2e9b4f06f3156cc22e4b234c8ccc0b1087d98c
Haizlbliek <linus.denning@gmail.com>
2024-11-25 16:36:30 -0500

    Chapter 13 through page 203

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+    # For sending emails
+    config.action_mailer.delivery_method = :smtp
+
+    # Email settings based upon typical Gmail settings
+    config.action_mailer.stmp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        domain: "domain.of.sender.net",
+        authentication: "plain",
+        user_name: "dave",
+        password: "secret",
+        enable_starttls_auto: true
+    }
+end
