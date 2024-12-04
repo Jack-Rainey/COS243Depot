53eba8acfc04d0269607d7a5fa321c51e732cd2a
Haizlbliek <linus.denning@gmail.com>
2024-12-04 14:15:36 -0500

    Chapter 14 Through I3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,22 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def login_as(user)
+    if respond_to? :visit
+      visit login_url
+      fill_in :name, with: user.name
+      fill_in :password, with: 'secret'
+      click_on 'Login'
+    else
+      pst login_url, params: { name: user.name, password: 'secret' }
+    end
+  end
+
+  def logout
+    delete logout_url
+  end
+
+  def Setup
+    login_as users(:one)
+  end
 end
