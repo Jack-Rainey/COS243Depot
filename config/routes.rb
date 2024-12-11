4d3bfc0b5541050e7ce4422b1b09f3665fc42fca
Haizlbliek <linus.denning@gmail.com>
2024-12-10 19:10:31 -0500

    Final Depot COS243 Submission

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,23 @@ Rails.application.routes.draw do
   end
   get 'sessions/create'
   get 'sessions/destroy'
+
+  # START_HIGHLIGHT
+  resources :support_requests, only: %i[ index update]
+  # END_HIGHLIGHT
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-  resources :products
+  resources :products do
+    get :who__bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
